eacac151c6c6df8b0b39fc3850156ffef1e46235
Eldos Yksanov <eldosyksanov@gmail.com>
2024-09-22 22:06:13 +0600

    create new solution LessonThread

---
+++ LessonThread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using System.Threading.Channels;
+
+namespace LessonThread;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
